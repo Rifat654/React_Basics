c736572258e26c20e403a4f0dd460b30fcf75dcc
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-04-03 21:07:48 +0600

    first commit

---
+++ React_Basics/src/App.jsx
@@ -0,0 +1,15 @@
+import State from "./components/state"
+import "./index.css"
+
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <State />
+    </div>
+  )
+}
+
+export default App
---
+++ React_Basics/src/components/State.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const State = () => {
+    return (
+        <div>
+            <h1>This is State Component</h1>
+        </div>
+    )
+}
+
+export default State
