b5099ab2472bef3121db6fa8da9c5166992d7a9b
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-04-18 23:46:09 +0600

    added tailwind css

--- Projects/src/App.jsx
+++ Projects/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1 className='text-center text-red-500 text-2xl'>Hello world</h1>
     </div>
   )
 }
