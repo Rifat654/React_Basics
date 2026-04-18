76d1d5df033dae75a46f4224ba27e9d8b28d5096
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-04-19 00:12:16 +0600

    added

--- Projects/src/components/Section-1/Navbar.jsx
+++ Projects/src/components/Section-1/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='flex items-center justify-between px-5'>
+            <h1 className='H'>NAVBAR</h1>
+            <div>
+                <ul className='flex gap-5'>
 
+                    <li className='text-blue-500 font-bold'>Home</li>
+                    <li className='text-blue-500 font-bold'>About</li>
+                    <li className='text-blue-500 font-bold'>Contact</li>
+
+                </ul>
+            </div>
         </div>
     )
 }
--- Projects/src/components/Section-1/Section_1.jsx
+++ Projects/src/components/Section-1/Section_1.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import Navbar from './Navbar'
 const Section_1 = () => {
     return (
         <div className='h-screen w-full bg-purple-950 text-white'>
-            <h1>Section:1</h1>
+            <Navbar />
+
+
 
         </div>
     )
