e0b0f8be1a3ec494bd57e322aa923cdf7df8ab26
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-05-04 23:02:09 +0600

    added

--- Projects/src/components/UseState.jsx
+++ Projects/src/components/UseState.jsx
@@ -2,32 +2,12 @@ import { useState } from 'react'
 
 const UseState = () => {
 
-
-    const [count, setCount] = useState(0);
-
-    function Increase() {
-        setCount(count + 1);
-    }
-
-    function Decrease() {
-        setCount(count - 1)
-    }
-
-    function Reset() {
-        setCount()
-    }
+    const [num, setNum] = useState(0);
 
     return (
         <div className='flex w-full h-screen justify-center items-center flex-col gap-5 '>
-            <h1>Value of {count}</h1>
-
-            <button onClick={Increase} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-                Click me
-            </button>
-            <button onClick={Decrease} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-                Click me
-            </button>
-            <button onClick={Reset}>Reset</button>
+            <h1>Value is {num}</h1>
+            <button onClick={() => setNum(num + 1)}>Increase</button>
         </div>
     )
 }
