d71d09492a1eec80b2402523ef1c247e619ce3ae
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-05-22 22:50:34 +0600

    added

--- React_Basics/src/App.jsx
+++ React_Basics/src/App.jsx
@@ -1,32 +1,27 @@
-import Card from "./components/Card"
-import State from "./components/State"
-import "./index.css"
-// import { list } from "./data.js"
-import PopUp from "./components/Pop_Up/PopUp.jsx"
-import PopupText from "./components/Pop_Up/PopupText.jsx"
-import Img_Slider from "./components/Img_Slider.jsx"
-import Form from "./components/Form.jsx"
-
+import { Link, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 
 const App = () => {
 
 
   return (
     <div className="   w-full h-screen" >
-      <h1 className="text-center">Basic React.js Concepts</h1>
-      {/* <div className="main">
-        {
-          list.map((item) => (
-            <Card key={item.id} name={item.name} age={item.age} />
+      <nav className="w-full h-16 bg-gray-200 flex items-center justify-between px-4" >
+        <h1>Logo</h1>
+        <ul className=" hidden lg:flex space-x-4 items-center" >
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
 
-          ))
-        };
-
-      </div> */}
-      {/* Custom Model Component */}
-      {/* <PopupText /> */}
-      {/* <State /> */}
-      <Form />
 
     </div>
   )
