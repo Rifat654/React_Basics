479ce1f3f15f5c52478be33b3753c3f8e7c53fe0
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-06-20 19:48:54 +0600

    added

--- React_Basics/src/App.jsx
+++ React_Basics/src/App.jsx
@@ -1,39 +1,11 @@
-import { Link, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Btn from "./components/Context-Exm/Btn"
-import Text from "./components/Context-Exm/Text"
+import React from 'react'
+import Use_Reduce from './components/UseReducer/Use_Reduce'
 
 const App = () => {
-
-
-
   return (
-    <div className="   w-full h-screen bg-blue-950 text-white flex items-center justify-center" >
-
-      {/* <nav className="w-full h-16 bg-slate-800 text-white items-center justify-between px-4" >
-        <h1>Logo</h1>
-        <ul className=" hidden lg:flex space-x-4 items-center" >
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-
-
-      </Routes> */}
-
-      {/* Context API  */}
-
-      {/* <h1 className="flex items-center justify-center text-4xl">Context API</h1> */}
-      <Btn />
-      <Text />
-
+    <div>
+      {/* React UseReducer Hook */}
+      <Use_Reduce />
     </div>
   )
 }
---
+++ React_Basics/src/components/UseReducer/Use_Reduce.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Use_Reduce = () => {
+    return (
+        <div>
+            <h1>UseReducer Hook Example</h1>
+        </div>
+    )
+}
+
+export default Use_Reduce
