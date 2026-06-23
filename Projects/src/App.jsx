b344a197a6903f37bcb9d1a9eacff9fccb27c770
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-06-23 18:28:10 +0600

    working with Accordion compoent

--- Projects/src/App.jsx
+++ Projects/src/App.jsx
@@ -1,15 +1,15 @@
+import Accordion from "./Sections/Accordion/Accordion"
 import Demo from "./Sections/Gallery-Project/Demo"
-
-
 
 const App = () => {
 
 
   return (
     <div >
-
+      {/* Accordion */}
+      <Accordion />
       {/* Gallery Project */}
-      <Demo />
+      {/* <Demo /> */}
 
     </div>
   )
---
+++ Projects/src/Sections/Accordion/Accordion.jsx
@@ -0,0 +1,11 @@
+
+
+const Accordion = () => {
+    return (
+        <div>
+            <h1>FAQ Section </h1>
+        </div>
+    )
+}
+
+export default Accordion
